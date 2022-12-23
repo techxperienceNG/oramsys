90b2c3abf2e4441e77fc807b22c80593b88256e7
Bruno Austin <brunoaugustine2@gmail.com>
2022-12-23 08:52:07 +0100

    add url change

--- frontend/src/config/API/api-prod.js
+++ frontend/src/config/API/api-prod.js
@@ -1,5 +1,5 @@
 const protocol = 'http';
-const host = 'https://oramsys-backend.vercel.app/';
+const host = 'https://oramsysapi.myjfh.com/https://oramsysapi.myjfh.com/';
 // const host = 'http://192.168.1.52:5002/';
 // const port = '5002';
 // const trailUrl = 'api/v1';

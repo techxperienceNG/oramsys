d07a7c3e1c769af803fb86b349862621e57b4fd8
bruno <brunoaugustine2@gmail.com>
2024-04-13 17:37:08 +0100

    add files exclude to github yml file

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5002
 
 app.use(cors())
-app.use(express.json({ limit: '50mb' }))
+app.use(express.json({ limit: '50mb' })) 
 app.use(express.static('files'))
 
 app.all("*", function (req, res, next) {
